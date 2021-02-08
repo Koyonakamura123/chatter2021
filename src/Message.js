3d2b215d7e31fd96e3b5074d933e98d04e1ebab4
Koyo Nakamura <koyo@uw.edu>
2021-02-07 20:49:22 -0800

    added username

--- src/Message.js
+++ src/Message.js
@@ -1,10 +1,17 @@
 
 function Message(props){
+    let username = "Anonymous";
+    if (props.name !== '') {
+      username = props.name;
+    }
     return <div className="message-row">
-    <div className="message">
-      {props.text}
+      <div className="text-username">
+        {username}
+      </div>
+      <div className="message">
+        {props.text}
+      </div>
     </div>
-  </div>
 }
 
 export default Message
